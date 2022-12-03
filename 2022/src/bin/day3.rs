e5b4205ee0ebf0ec980179b54f66f3761e0b4b2c
ryanprice <rjprice04@gmail.com>
2022-12-03 12:47:43 -0600

    refactor: Move Parts into Functions and Add Tests

--- 2022/src/bin/day3.rs
+++ 2022/src/bin/day3.rs
@@ -3,43 +3,13 @@ use itertools::Itertools;
 fn main() {
     let items = include_str!("./input/input_3.prod").lines().collect::<Vec<&str>>();
     
-    let part_1 = items
-                    .iter()
-                    .map(|x| {
-                        let len = x.len();
-                        let first_half = x.get(0..len/2).unwrap();
-                        let second_half = x.get((len/2)..).unwrap();
+    let part_1 = part_1_function(&items);
 
-                        (first_half, second_half)
-                    })
-                    .map(|(first, second)| {
-                        first
-                            .chars()
-                            .filter(|x| second.contains(*x))
-                            .nth(0)
-                            .unwrap()
-                    })
-                    .map(|x| get_priority(x))
-                    .sum::<u32>();
-
-    let part_2 = items
-                    .iter()
-                    .chunks(3)
-                    .into_iter()
-                    .map(|x| {
-                        let mut values = x.into_iter();
-                        let first = values.next().unwrap();
-                        let second = values.next().unwrap();
-                        let third = values.next().unwrap();
-                        first.chars().filter(|x| second.contains(*x) && third.contains(*x)).nth(0).unwrap()
-
-                    })
-                    .map(|x| get_priority(x))
-                    .sum::<u32>();
+    let part_2 = part_2_function(&items);
 
 
-    println!("part_1 {:?}", part_1);
-    println!("part_2 {:?}", part_2);
+    println!("part_1 {}", part_1);
+    println!("part_2 {}", part_2);
 }
 
 fn get_priority(character: char) -> u32 {
@@ -59,3 +29,73 @@ fn get_priority(character: char) -> u32 {
         character as u32 - 96
     }
 }
+
+fn part_1_function(input: &Vec<&str>) -> u32 {
+    input
+        .iter()
+        .map(|x| {
+            let len = x.len();
+            let first_half = x.get(0..len/2).unwrap();
+            let second_half = x.get((len/2)..).unwrap();
+
+            (first_half, second_half)
+        })
+    .map(|(first, second)| {
+        first
+            .chars()
+            .filter(|x| second.contains(*x))
+            .nth(0)
+            .unwrap()
+    })
+    .map(|x| get_priority(x))
+        .sum::<u32>()
+}
+
+
+fn part_2_function(input: &Vec<&str>) -> u32 {
+    input
+        .iter()
+        .chunks(3)
+        .into_iter()
+        .map(|x| {
+            let mut values = x.into_iter();
+            let first = values.next().unwrap();
+            let second = values.next().unwrap();
+            let third = values.next().unwrap();
+            first.chars().filter(|x| second.contains(*x) && third.contains(*x)).nth(0).unwrap()
+
+        })
+    .map(|x| get_priority(x))
+        .sum::<u32>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn part_1() {
+
+        assert_eq!(part_1_function(&INPUT.lines().collect::<Vec<&str>>()), 157);
+    }
+    #[test]
+    fn part_2() {
+
+        assert_eq!(part_2_function(&INPUT.lines().collect::<Vec<&str>>()), 70);
+    }
+}
+
+
+
+
+
+
+
+
